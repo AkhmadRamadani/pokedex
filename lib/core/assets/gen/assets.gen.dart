c6c70d30fd52f84bbfe4140efc4a7da547ebd913
Akhmad Ramadani <akhmdramadani18@gmail.com>
2025-05-25 00:32:36 +0700

    🍱 assets: generate new asset

--- lib/core/assets/gen/assets.gen.dart
+++ lib/core/assets/gen/assets.gen.dart
@@ -13,6 +13,13 @@ import 'package:flutter_svg/flutter_svg.dart' as _svg;
 import 'package:lottie/lottie.dart' as _lottie;
 import 'package:vector_graphics/vector_graphics.dart' as _vg;
 
+class $AssetsImagesGen {
+  const $AssetsImagesGen();
+
+  /// Directory path: assets/images/dummies
+  $AssetsImagesDummiesGen get dummies => const $AssetsImagesDummiesGen();
+}
+
 class $AssetsLottiesGen {
   const $AssetsLottiesGen();
 
@@ -32,6 +39,16 @@ class $AssetsSvgGen {
 
   /// Directory path: assets/svg/icons
   $AssetsSvgIconsGen get icons => const $AssetsSvgIconsGen();
+}
+
+class $AssetsImagesDummiesGen {
+  const $AssetsImagesDummiesGen();
+
+  /// File path: assets/images/dummies/charizard.png
+  AssetGenImage get charizard => const AssetGenImage('assets/images/dummies/charizard.png');
+
+  /// List of all assets
+  List<AssetGenImage> get values => [charizard];
 }
 
 class $AssetsSvgElementsGen {
@@ -96,29 +113,32 @@ class $AssetsSvgElementsGen {
 
   /// List of all assets
   List<SvgGenImage> get values => [
-        dragonElement,
-        electricElement,
-        fairyElement,
-        fighterElement,
-        fireElement,
-        flyingElement,
-        ghostElement,
-        grassElement,
-        iceElement,
-        insectElement,
-        metalElement,
-        nocturnalElement,
-        normalElement,
-        poisonousElement,
-        psychicElement,
-        stoneElement,
-        terrestrialElement,
-        waterElement
-      ];
+    dragonElement,
+    electricElement,
+    fairyElement,
+    fighterElement,
+    fireElement,
+    flyingElement,
+    ghostElement,
+    grassElement,
+    iceElement,
+    insectElement,
+    metalElement,
+    nocturnalElement,
+    normalElement,
+    poisonousElement,
+    psychicElement,
+    stoneElement,
+    terrestrialElement,
+    waterElement,
+  ];
 }
 
 class $AssetsSvgIconsGen {
   const $AssetsSvgIconsGen();
+
+  /// File path: assets/svg/icons/arrow_down_icon.svg
+  SvgGenImage get arrowDownIcon => const SvgGenImage('assets/svg/icons/arrow_down_icon.svg');
 
   /// File path: assets/svg/icons/category_icon.svg
   SvgGenImage get categoryIcon => const SvgGenImage('assets/svg/icons/category_icon.svg');
@@ -164,46 +184,115 @@ class $AssetsSvgIconsGen {
   SvgGenImage get pokedexInactiveIcon =>
       const SvgGenImage('assets/svg/icons/pokedex_inactive_icon.svg');
 
+  /// File path: assets/svg/icons/search_icon.svg
+  SvgGenImage get searchIcon => const SvgGenImage('assets/svg/icons/search_icon.svg');
+
   /// File path: assets/svg/icons/weight_icon.svg
   SvgGenImage get weightIcon => const SvgGenImage('assets/svg/icons/weight_icon.svg');
 
   /// List of all assets
   List<SvgGenImage> get values => [
-        categoryIcon,
-        favoriteBottomNavBarActiveIcon,
-        favoriteBottomNavBarInactiveIcon,
-        favoriteCardActiveIcon,
-        favoriteCardInactiveIcon,
-        favoriteDetailActiveIcon,
-        favoriteDetailInactiveIcon,
-        femaleIcon,
-        heightIcon,
-        maleIcon,
-        pokedexActiveIcon,
-        pokedexInactiveIcon,
-        weightIcon
-      ];
+    arrowDownIcon,
+    categoryIcon,
+    favoriteBottomNavBarActiveIcon,
+    favoriteBottomNavBarInactiveIcon,
+    favoriteCardActiveIcon,
+    favoriteCardInactiveIcon,
+    favoriteDetailActiveIcon,
+    favoriteDetailInactiveIcon,
+    femaleIcon,
+    heightIcon,
+    maleIcon,
+    pokedexActiveIcon,
+    pokedexInactiveIcon,
+    searchIcon,
+    weightIcon,
+  ];
 }
 
 class Assets {
   const Assets._();
 
+  static const $AssetsImagesGen images = $AssetsImagesGen();
   static const $AssetsLottiesGen lotties = $AssetsLottiesGen();
   static const $AssetsSvgGen svg = $AssetsSvgGen();
 }
 
-class SvgGenImage {
-  const SvgGenImage(
-    this._assetName, {
-    this.size,
-    this.flavors = const {},
-  }) : _isVecFormat = false;
+class AssetGenImage {
+  const AssetGenImage(this._assetName, {this.size, this.flavors = const {}});
 
-  const SvgGenImage.vec(
-    this._assetName, {
-    this.size,
-    this.flavors = const {},
-  }) : _isVecFormat = true;
+  final String _assetName;
+
+  final Size? size;
+  final Set<String> flavors;
+
+  Image image({
+    Key? key,
+    AssetBundle? bundle,
+    ImageFrameBuilder? frameBuilder,
+    ImageErrorWidgetBuilder? errorBuilder,
+    String? semanticLabel,
+    bool excludeFromSemantics = false,
+    double? scale,
+    double? width,
+    double? height,
+    Color? color,
+    Animation<double>? opacity,
+    BlendMode? colorBlendMode,
+    BoxFit? fit,
+    AlignmentGeometry alignment = Alignment.center,
+    ImageRepeat repeat = ImageRepeat.noRepeat,
+    Rect? centerSlice,
+    bool matchTextDirection = false,
+    bool gaplessPlayback = true,
+    bool isAntiAlias = false,
+    String? package,
+    FilterQuality filterQuality = FilterQuality.medium,
+    int? cacheWidth,
+    int? cacheHeight,
+  }) {
+    return Image.asset(
+      _assetName,
+      key: key,
+      bundle: bundle,
+      frameBuilder: frameBuilder,
+      errorBuilder: errorBuilder,
+      semanticLabel: semanticLabel,
+      excludeFromSemantics: excludeFromSemantics,
+      scale: scale,
+      width: width,
+      height: height,
+      color: color,
+      opacity: opacity,
+      colorBlendMode: colorBlendMode,
+      fit: fit,
+      alignment: alignment,
+      repeat: repeat,
+      centerSlice: centerSlice,
+      matchTextDirection: matchTextDirection,
+      gaplessPlayback: gaplessPlayback,
+      isAntiAlias: isAntiAlias,
+      package: package,
+      filterQuality: filterQuality,
+      cacheWidth: cacheWidth,
+      cacheHeight: cacheHeight,
+    );
+  }
+
+  ImageProvider provider({AssetBundle? bundle, String? package}) {
+    return AssetImage(_assetName, bundle: bundle, package: package);
+  }
+
+  String get path => _assetName;
+
+  String get keyName => _assetName;
+}
+
+class SvgGenImage {
+  const SvgGenImage(this._assetName, {this.size, this.flavors = const {}}) : _isVecFormat = false;
+
+  const SvgGenImage.vec(this._assetName, {this.size, this.flavors = const {}})
+    : _isVecFormat = true;
 
   final String _assetName;
   final Size? size;
@@ -232,11 +321,7 @@ class SvgGenImage {
   }) {
     final _svg.BytesLoader loader;
     if (_isVecFormat) {
-      loader = _vg.AssetBytesLoader(
-        _assetName,
-        assetBundle: bundle,
-        packageName: package,
-      );
+      loader = _vg.AssetBytesLoader(_assetName, assetBundle: bundle, packageName: package);
     } else {
       loader = _svg.SvgAssetLoader(
         _assetName,
@@ -269,10 +354,7 @@ class SvgGenImage {
 }
 
 class LottieGenImage {
-  const LottieGenImage(
-    this._assetName, {
-    this.flavors = const {},
-  });
+  const LottieGenImage(this._assetName, {this.flavors = const {}});
 
   final String _assetName;
   final Set<String> flavors;
@@ -289,11 +371,7 @@ class LottieGenImage {
     _lottie.LottieImageProviderFactory? imageProviderFactory,
     Key? key,
     AssetBundle? bundle,
-    Widget Function(
-      BuildContext,
-      Widget,
-      _lottie.LottieComposition?,
-    )? frameBuilder,
+    Widget Function(BuildContext, Widget, _lottie.LottieComposition?)? frameBuilder,
     ImageErrorWidgetBuilder? errorBuilder,
     double? width,
     double? height,
