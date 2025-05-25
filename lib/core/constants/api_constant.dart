60c234742bac71278529431f554c33b0bbb1c694
Akhmad Ramadani <akhmdramadani18@gmail.com>
2025-05-25 18:09:37 +0700

    ✏️ typo: fix route typo

--- lib/core/constants/api_constant.dart
+++ lib/core/constants/api_constant.dart
@@ -1,6 +1,7 @@
 class ApiConstant {
   ApiConstant._();
-  static const String baseUrl = 'https://gist.githubusercontent.com/hungps/0bfdd96d3ab9ee20c2e572e47c6834c7/raw/';
+  static const String baseUrl =
+      'https://gist.githubusercontent.com/hungps/0bfdd96d3ab9ee20c2e572e47c6834c7/raw/';
 
-  static const String pokemonList = 'pokemon.json';
+  static const String pokemonList = 'pokemons.json';
 }
